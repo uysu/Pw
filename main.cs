b25dd7f177c7c0daaf0fef572f5e22fc3adc6342
ysu <136ysu@gmail.com>
2023-02-27 05:08:41 +0300

    Once time but harder

--- main.cs
+++ main.cs
@@ -1,11 +1,16 @@
+using (System);
+
 namespace Git
 {
     class Git
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello Universe!")
+            System.Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            System.Console.WriteLine(name.Length); //debug line
+            System.Console.WriteLine($"Hello {name}!");
         }
-        
     }
+
 }
